b7a8a92df497e593717e9c27db14a3cd577f6dbf
saimkhan <msaimkhan2233@gmail.com>
2026-02-08 11:25:51 +0500

    dark theme addition

---
+++ app.js
@@ -0,0 +1,11 @@
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        themeIcon.innerText = '☀️';
+    } else {
+        themeIcon.innerText = '🌙';
+    }
+});
